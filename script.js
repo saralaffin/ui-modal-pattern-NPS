37d6b293915f10b7a74f9e961544c7064a5e098f
Sara Laffin <sarala@umich.edu>
2020-02-06 14:51:44 -0500

    add park photo to active div

--- script.js
+++ script.js
@@ -17,11 +17,12 @@ function hidden(eve) {
     // console.dir(eve.path[1])
     eve.path[1].classList.toggle("hidden")
 }
-
-let url = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=KDAvx939Yj9FsDsKrBSHeZi7BMzZHUqpQdLU5tfV"
+// curl -X GET "https://developer.nps.gov/api/v1/parks?limit=1" -H "accept: application/json"
+let url = "https://developer.nps.gov/api/v1/parks?limit=20&fields=images&api_key=KDAvx939Yj9FsDsKrBSHeZi7BMzZHUqpQdLU5tfV"
 fetch(url)
     .then(res => res.json())
     .then(res => {
-        console.log(res.data[0].fullName)
+        console.log(res.data[0].images[0].url)
         document.querySelector(".active-park").innerHTML = res.data[0].fullName
+        document.querySelector(".active-img").src = res.data[0].images[0].url
     } )
