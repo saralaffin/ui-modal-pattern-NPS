eec09bebba48178b505faae050e345c0ed13f6d5
Sara Laffin <sarala@umich.edu>
2020-02-06 15:54:15 -0500

    add park titles to background divs

--- script.js
+++ script.js
@@ -20,10 +20,18 @@ function hidden(eve) {
 
 
 let url = "https://developer.nps.gov/api/v1/parks?limit=20&fields=images&api_key=KDAvx939Yj9FsDsKrBSHeZi7BMzZHUqpQdLU5tfV"
+
 fetch(url)
     .then(res => res.json())
     .then(res => {
-        console.log(res.data[0].description)
+        
+        for (let i = 0; i < 20; i++) {
+            let park = document.createElement("div")
+            park.setAttribute('class', 'parks')
+            park.innerHTML = res.data[i].fullName
+            document.querySelector(".all-parks").appendChild(park)
+            // document.querySelector(".active-img").src = res.data[0].images[0].url
+        }
         document.querySelector(".active-park").innerHTML = res.data[0].fullName
         document.querySelector(".active-img").src = res.data[0].images[0].url
         document.querySelector(".active-description").innerHTML = res.data[0].description
