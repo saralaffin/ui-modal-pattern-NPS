24efea9bab851a79222db73999c030c7024607e2
Sara Laffin <sarala@umich.edu>
2020-02-06 15:40:29 -0500

    format active div and background divs

--- script.js
+++ script.js
@@ -17,12 +17,14 @@ function hidden(eve) {
     // console.dir(eve.path[1])
     eve.path[1].classList.toggle("hidden")
 }
-// curl -X GET "https://developer.nps.gov/api/v1/parks?limit=1" -H "accept: application/json"
+
+
 let url = "https://developer.nps.gov/api/v1/parks?limit=20&fields=images&api_key=KDAvx939Yj9FsDsKrBSHeZi7BMzZHUqpQdLU5tfV"
 fetch(url)
     .then(res => res.json())
     .then(res => {
-        console.log(res.data[0].images[0].url)
+        console.log(res.data[0].description)
         document.querySelector(".active-park").innerHTML = res.data[0].fullName
         document.querySelector(".active-img").src = res.data[0].images[0].url
+        document.querySelector(".active-description").innerHTML = res.data[0].description
     } )
